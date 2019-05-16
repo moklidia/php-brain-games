6cbffe89cf09ac5f83094b0c8a70789c38c2a26d
moklidia <kitsunegakkou@gmail.com>
2019-05-16 11:06:21 +0800

    add brain-gcd

---
+++ src/Games/Gcd.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BrainGames\Games\Gcd;
+
+use function BrainGames\Utils\getRandNum;
+
+use function BrainGames\Games\Flow\playGame;
+
+function findGcd($num1, $num2)
+{
+    return gmp_gcd($num1, $num2);
+}
+
+function playGcd()
+{
+    $task = 'Find the greatest common divisor of given numbers.';
+    $gameRules = function () {
+        $num1 = getRandNum();
+        $num2 = getRandNum();
+        $question = "Question: {$num1} {$num2}";
+        $correctAnswer = findGcd($num1, $num2);
+        $rules['question'] = $question;
+        $rules['correct_answer'] = $correctAnswer;
+        return $rules;
+    };
+    return playGame($task, $gameRules);
+}
